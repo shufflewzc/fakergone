d9293a5872d2551aa05aaac73fec81ceb2b87c9d
wenyuling <wenyuing10@163.com>
2021-09-21 17:20:40 +0800

    修复bug

--- jd_health.js
+++ jd_health.js
@@ -24,6 +24,8 @@ const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
 const notify = $.isNode() ? require('./sendNotify') : "";
 let cookiesArr = [], cookie = "", allMessage = "", message;
 const inviteCodes = [
+  '',
+  ''
 ]
 let reward = $.isNode() ? (process.env.JD_HEALTH_REWARD_NAME ? process.env.JD_HEALTH_REWARD_NAME : '') : ($.getdata('JD_HEALTH_REWARD_NAME') ? $.getdata('JD_HEALTH_REWARD_NAME') : '');
 const randomCount = $.isNode() ? 20 : 5;
