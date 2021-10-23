9beaa535da32bf9bf75ba36fa494b875ce69dfa0
xumf <xumf.net@gmail.com>
2021-10-23 19:34:22 +0800

    update

--- jd_lzdz2_fashion.js
+++ jd_lzdz2_fashion.js
@@ -28,12 +28,7 @@ if ($.isNode()) {
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
         return;
     }
-    authorCodeList = await getAuthorCodeList('https://gitee.com/fatelight/dongge/raw/master/dongge/lzdz2_fashion.json')
-    if(authorCodeList === '404: Not Found'){
-        authorCodeList = [
-            'b3b3f59c9015493a9d8e2f1d5d9b92ea',
-        ]
-    }
+    authorCodeList = []
     for (let i = 0; i < cookiesArr.length; i++) {
         if (cookiesArr[i]) {
             cookie = cookiesArr[i]
