8a0947a9084a9e0b7588bb86c1251949cf3e37e5
xumf <xumf@ligeit.com>
2021-10-26 13:36:00 +0800

    update

--- jd_fcdyj.js
+++ jd_fcdyj.js
@@ -85,7 +85,7 @@ const JD_API_HOST = `https://api.m.jd.com`;
         }
     }
     if (new Date().getHours() >= 10) {
-        await getAuthorShareCode()
+        // await getAuthorShareCode()
         if ($.authorCode && $.authorCode.length) {
             for (let i = 0; i < cookiesArr.length; i++) {
                 cookie = cookiesArr[i];
