8f9794d7d93627e45f0f4eae77366c7e73efab0c
xumf <xumf.net@gmail.com>
2021-11-03 23:28:16 +0800

    update

--- jd_redPacket_help.js
+++ jd_redPacket_help.js
@@ -41,7 +41,7 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
-  let res = await getAuthorShareCode('')
+  let res = []
   $.authorMyShareIds = [...(res || [])];
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
